b2114e3efa5efcb38243a8eedbe8564bd7911cbe
Alex Karpodinis <alexander@karpodinis.net>
2020-04-09 17:10:04 -0400

    Adding favicon

--- aisles.py
+++ aisles.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from os import path
+
+from flask import Flask, render_template, send_from_directory
 
 from cache import cache
 
@@ -14,3 +16,9 @@ def index():
     list_status = 'Found a list!' if lists else 'No lists yet!'
 
     return render_template('status.html', list_status=list_status, lists=lists)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(path.join(app.root_path, 'static'),
+                               'images/favicon.ico', mimetype='image/vnd.microsoft.icon')
